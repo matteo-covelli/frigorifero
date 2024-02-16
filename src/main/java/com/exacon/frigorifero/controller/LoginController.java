ca6655c5b6c9b91ae516993e8ae863a1be1c1414
Matteo Covelli <matteo.covelli@exacon.it>
2024-02-16 14:03:12 +0100

    fixed import

--- src/main/java/com/exacon/frigorifero/controller/LoginController.java
+++ src/main/java/com/exacon/frigorifero/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.exacon.frigorifero.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
